da7daa6a9559a9dcc856fd97b207d89c6d46d35f
John Wang <johncwang@gmail.com>
2016-12-28 01:04:40 -0800

    update test

--- test/test_request.rb
+++ test/test_request.rb
@@ -9,16 +9,19 @@ require 'test/unit'
 class RetryParamsTest < Test::Unit::TestCase
   def test_request
     stubs = Faraday::Adapter::Test::Stubs.new do |stub|
+      stub.get('/200') { |env| [200, {}, 'success'] }
       stub.get('/429') { |env| [429, {}, 'too many requests'] }
     end
 
     @conn = Faraday.new 'http://example.com/api' do |builder|
       builder.use FaradayMiddleware::Request::Retry
       builder.adapter :test, stubs do |stub|
+        stub.get('/200') { |env| [200, {}, 'success'] }
         stub.get('/429') { |env| [429, {}, 'too many requests'] }
       end
     end
 
+    @conn.get '/200'
     @conn.get '/429'
   end
 end
