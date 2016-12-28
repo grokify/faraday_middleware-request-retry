93302ce476f83c59d792ef9f1aa15e011fa33302
John Wang <johncwang@gmail.com>
2016-12-28 01:02:16 -0800

    add test

---
+++ test/test_request.rb
@@ -0,0 +1,24 @@
+require 'coveralls'
+Coveralls.wear!
+
+require 'faraday'
+require 'faraday_middleware'
+require 'faraday_middleware-request-retry'
+require 'test/unit'
+
+class RetryParamsTest < Test::Unit::TestCase
+  def test_request
+    stubs = Faraday::Adapter::Test::Stubs.new do |stub|
+      stub.get('/429') { |env| [429, {}, 'too many requests'] }
+    end
+
+    @conn = Faraday.new 'http://example.com/api' do |builder|
+      builder.use FaradayMiddleware::Request::Retry
+      builder.adapter :test, stubs do |stub|
+        stub.get('/429') { |env| [429, {}, 'too many requests'] }
+      end
+    end
+
+    @conn.get '/429'
+  end
+end
